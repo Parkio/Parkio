1ccbfa8c780e5eedffaa98dcc66cdf4601de8690
TMullCoding <thomasmullenwork@gmail.com>
2016-03-09 23:04:52 -0500

    remove sorting

--- ParkioExperimental/src/Sort.java
+++ ParkioExperimental/src/Sort.java
@@ -5,21 +5,7 @@ class Sort {
 	static ParkingSpot[] sortByDistance(double lat, double lng, ParkingSpot[] spots){
 		ParkingSpot[] sortedSpots = spots;
 		
-		//DELETE THIS AND USE REAL SORTING
-		class RadiusComparator implements Comparator<ParkingSpot>{
-			private double lat, lng;
-			public RadiusComparator(double lat, double lng){
-				this.lat=lat;
-				this.lng=lng;
-			}
-			
-			@Override
-			public int compare(ParkingSpot o1, ParkingSpot o2) {
-				return (int)(o1.distanceToPoint(lat,lng) - o2.distanceToPoint(lat,lng));
-			}
-		}
-		Arrays.sort(sortedSpots, new RadiusComparator(lat,lng));
-		//END OF DELETE THIS
+
 		
 		return sortedSpots;
 	}
