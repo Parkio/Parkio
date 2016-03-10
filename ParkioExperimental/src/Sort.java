dc9552bbf6c59cf6445222b0b73391397e4f345f
TMullCoding <thomasmullenwork@gmail.com>
2016-03-09 23:05:28 -0500

    remove excess imports

--- ParkioExperimental/src/Sort.java
+++ ParkioExperimental/src/Sort.java
@@ -1,12 +1,7 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Sort {
 	static ParkingSpot[] sortByDistance(double lat, double lng, ParkingSpot[] spots){
 		ParkingSpot[] sortedSpots = spots;
-		
 
-		
 		return sortedSpots;
 	}
 }
