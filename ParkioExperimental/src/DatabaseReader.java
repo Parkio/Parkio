00804fb9aa618d09db26213e2eb559e13ddd9938
TMullCoding <thomasmullenwork@gmail.com>
2016-03-09 22:05:06 -0500

    database reader

--- ParkioExperimental/src/DatabaseReader.java
+++ ParkioExperimental/src/DatabaseReader.java
@@ -1,6 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class DatabaseReader {
-	Object[] read(int start, int end){ //Need to decide on a return type
-		return null;
+	public static ArrayList<ArrayList<String>> read(){ //Need to decide on a return type
+		
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>(); //Store csv data in a 2d array
+		BufferedReader reader = null; //Use a buffered reader to read
+		
+		try {
+			String line;
+			reader = new BufferedReader(new FileReader("data/subset.csv")); //Open the file and instantiate the reader
+			
+			while ((line = reader.readLine()) != null) { //Iterate across rows
+				result.add(parseCSVRow(line));	//Add the parsed row
+			}
+		} catch (IOException e) { //Exception when reading
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null){ 
+					reader.close(); //Close reader when finished
+				}
+			} catch (IOException e) { //Exception when closing reader
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+		
+	private static ArrayList<String> parseCSVRow(String csvRow) {
+		ArrayList<String> result = new ArrayList<String>(); //Store columns in arrayList
+		
+			String[] splitRow = csvRow.split("\\s*,\\s*");	//Split the row via regex
+			for (int i = 0; i < splitRow.length; i++) {		//Iterate across columns
+				result.add(splitRow[i].trim());				//Add column to row
+			}
+		
+		return result;
 	}
 }
